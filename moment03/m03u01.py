f5c9c7fada86d270b9f1ce156883fef9a0462ae0
oscarlundgren1 <92087289+oscarlundgren1@users.noreply.github.com>
2021-12-07 10:51:11 +0100

    Changes

---
+++ moment03/m03u01.py
@@ -0,0 +1,9 @@
+from time import localtime, strftime
+
+
+timme = (localtime())
+
+if timme != 17 and 18 and 19 and 20 and 21 and 22 and 23 and 24:
+    print("Skolan är inte slut ännu.")
+else:
+    print("Skolan är slut.")
